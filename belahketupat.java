ad89179815e42a8a7d36fdbe9f677169ee3a0a41
rienaagustina <48663767+rienaagstn@users.noreply.github.com>
2019-04-02 08:59:46 +0700

    Add files via upload

---
+++ belahketupat.java
@@ -0,0 +1,50 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jurnalriena;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Praktikan
+ */
+public class belahketupat {
+
+    /**
+     * @param args the command line arguments
+     */
+         
+   public static void main(String[] args) {
+       System.out.println("masukan angka : "); 
+       Scanner sc = new Scanner(System.in); 
+        int i; 
+        int n = sc.nextInt(); 
+ 
+        for (i = 0; i < (2 * n) + 1; i++) { 
+ 
+            for (int j = 0; j < (2 * n) + 1; j++) { 
+ 
+                if (i < n) { 
+                    if ((j > n - i) && (j < n + i)) { 
+                        System.out.print("*"); 
+                    } else { 
+                        System.out.print("0"); 
+                    } 
+                } else { 
+ 
+                    if ((j > i - n) && (j < 3*n-i)) { 
+                        System.out.print("*"); 
+                    } else { 
+                        System.out.print("0"); 
+                    } 
+ 
+                } 
+            } 
+            System.out.println(); 
+ 
+        } 
+    } 
+} 
